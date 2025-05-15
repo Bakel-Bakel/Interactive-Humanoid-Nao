9739bb5a8814a26e40ba5d4b6a246528001e8dea
bakel-bakel <begededum4bakel@gmail.com>
2025-05-15 04:48:45 +0200

    added nao dialog

--- translations/translation_en_US.ts
+++ translations/translation_en_US.ts
@@ -2,6 +2,190 @@
 <!DOCTYPE TS>
 <TS version="2.1" language="en_US">
     <context>
+        <name>behavior.xar:/Animated Say</name>
+        <message>
+            <source>Hello</source>
+            <comment>Text</comment>
+            <translation type="vanished">Hello</translation>
+        </message>
+        <message>
+            <source>What is going on</source>
+            <comment>Text</comment>
+            <translation type="obsolete">What is going on</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior.xar:/Say</name>
+        <message>
+            <source>Hello, my name is Nao</source>
+            <comment>Text</comment>
+            <translation type="obsolete">Hello, my name is Nao</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior.xar:/Say (1)</name>
+        <message>
+            <source>damn</source>
+            <comment>Text</comment>
+            <translation type="obsolete">damn</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior.xar:/Say (2)</name>
+        <message>
+            <source>What is your name, little one?</source>
+            <comment>Text</comment>
+            <translation type="obsolete">What is your name, little one?</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior.xar:/Say (3)</name>
+        <message>
+            <source>Hello, </source>
+            <comment>Text</comment>
+            <translation type="obsolete">Hello, </translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior.xar:/Say (4)</name>
+        <message>
+            <source>I am 22 years old</source>
+            <comment>Text</comment>
+            <translation type="obsolete">I am 22 years old</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior.xar:/Say (5)</name>
+        <message>
+            <source>How old are you?</source>
+            <comment>Text</comment>
+            <translation type="obsolete">How old are you?</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior.xar:/Say (6)</name>
+        <message>
+            <source>Oh, young one, you are</source>
+            <comment>Text</comment>
+            <translation type="obsolete">Oh, young one, you are</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior.xar:/Say (7)</name>
+        <message>
+            <source>How old are you?</source>
+            <comment>Text</comment>
+            <translation type="obsolete">How old are you?</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior.xar:/askAge</name>
+        <message>
+            <location filename="behavior.xar" line="0"/>
+            <source>How old are you?</source>
+            <comment>Text</comment>
+            <translation type="unfinished">How old are you?</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior.xar:/askColor</name>
+        <message>
+            <location filename="behavior.xar" line="0"/>
+            <source>What is your favourite color?</source>
+            <comment>Text</comment>
+            <translation type="unfinished">What is your favourite color?</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior.xar:/askFamily</name>
+        <message>
+            <location filename="behavior.xar" line="0"/>
+            <source>How many people are in your family?</source>
+            <comment>Text</comment>
+            <translation type="unfinished">How many people are in your family?</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior.xar:/askName</name>
+        <message>
+            <location filename="behavior.xar" line="0"/>
+            <source>What is your name, little one?</source>
+            <comment>Text</comment>
+            <translation type="unfinished">What is your name, little one?</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior.xar:/favColor</name>
+        <message>
+            <source>Well my favourite color is Red</source>
+            <comment>Text</comment>
+            <translation type="obsolete">Well my favourite color is Red</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior.xar:/hello</name>
+        <message>
+            <location filename="behavior.xar" line="0"/>
+            <source>Hello, </source>
+            <comment>Text</comment>
+            <translation type="unfinished">Hello, </translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior.xar:/sayAge</name>
+        <message>
+            <location filename="behavior.xar" line="0"/>
+            <source>I am 22 years old</source>
+            <comment>Text</comment>
+            <translation type="unfinished">I am 22 years old</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior.xar:/sayColor</name>
+        <message>
+            <location filename="behavior.xar" line="0"/>
+            <source>Well my favourite color is Red</source>
+            <comment>Text</comment>
+            <translation type="unfinished">Well my favourite color is Red</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior.xar:/sayFamily</name>
+        <message>
+            <location filename="behavior.xar" line="0"/>
+            <source>I am a Naon the Nao family, there is Nao V6, Nao V50 and Pepper robots</source>
+            <comment>Text</comment>
+            <translation type="unfinished">I am a Naon the Nao family, there is Nao V6, Nao V50 and Pepper robots</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior.xar:/sayName</name>
+        <message>
+            <location filename="behavior.xar" line="0"/>
+            <source>Hello, my name is Nao</source>
+            <comment>Text</comment>
+            <translation type="unfinished">Hello, my name is Nao</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior.xar:/sayYoung</name>
+        <message>
+            <location filename="behavior.xar" line="0"/>
+            <source>Oh, young one, you are</source>
+            <comment>Text</comment>
+            <translation type="unfinished">Oh, young one, you are</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior.xar:/sayYoung (1)</name>
+        <message>
+            <location filename="behavior.xar" line="0"/>
+            <source>The is a good bright color</source>
+            <comment>Text</comment>
+            <translation type="unfinished">The is a good bright color</translation>
+        </message>
+    </context>
+    <context>
         <name>behavior_1/behavior.xar:/Say</name>
         <message>
             <source>Hello</source>
@@ -9,10 +193,9 @@
             <translation type="vanished">Hello</translation>
         </message>
         <message>
-            <location filename="behavior_1/behavior.xar" line="0"/>
             <source>Hello, my name is Nao</source>
             <comment>Text</comment>
-            <translation type="unfinished">Hello, my name is Nao</translation>
+            <translation type="obsolete">Hello, my name is Nao</translation>
         </message>
     </context>
     <context>
@@ -23,10 +206,20 @@
             <translation type="vanished">Hello</translation>
         </message>
         <message>
-            <location filename="behavior_1/behavior.xar" line="0"/>
             <source>Sorry, I do not understand</source>
             <comment>Text</comment>
-            <translation type="unfinished">Sorry, I do not understand</translation>
+            <translation type="obsolete">Sorry, I do not understand</translation>
+        </message>
+        <message>
+            <location filename="../../../Downloads/Downloads/Downloads/Downloads/Downloads/Downloads/Downloads/Downloads/Downloads/Downloads/Downloads/Downloads/Downloads/Downloads/Downloads/Downloads/Downloads/Downloads/Downloads/Downloads/Downloads/Downloads/Downloads/Downloads/Downloads/Downloads/Downloads/Downloads/Downloads/Downloads/Downloads/Downloads/Downloads/Downloads/Downloads/Downloads/Downloads/Downloads/Downloads/Downloads/Downloads/Downloads/Downloads/Downloads/Downloads/Downloads/Downloads/Downloads/Downloads/Downloads/Downloads/Downloads/Downloads/Downloads/Downloads/Downloads/Downloads/Downloads/Downloads/Downloads/Downloads/choregraphe-2.8.8-ubuntu2204-standalone/choregraphe-2.8.8-ubuntu2204/translations/behavior_1/behavior.xar" line="0"/>
+            <source></source>
+            <comment>Text</comment>
+            <translation></translation>
+        </message>
+        <message>
+            <source>damn</source>
+            <comment>Text</comment>
+            <translation type="obsolete">damn</translation>
         </message>
     </context>
 </TS>
